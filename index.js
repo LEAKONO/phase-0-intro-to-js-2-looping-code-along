cc2e6139b49f82a6d5b758903e70c62e8a3a68ca
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-15 00:46:04 +0300

    Loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let cardsArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        cardsArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cardsArray;
+}
+function countDown(number) {
+    if (number < 0) {
+        console.log(10);
+        return;
+    }
+
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
